86ca411d84da11952531cb35bf36e9c4a728633e
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-25 19:21:22 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,7 +6,7 @@ def array_with_two_elements
   my_two_array = ["el1","el2"]
 end
 
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
-def first_element
-  
+def first_element (array)
+  array[0]
+end
